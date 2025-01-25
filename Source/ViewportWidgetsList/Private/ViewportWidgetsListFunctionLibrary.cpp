04d39cae26eddb6aa86fee2b5a3e33b23a745a61
mori <kiruru002@gmail.com>
2025-01-26 00:37:57 +0900

    WidgetTree check

--- Source/ViewportWidgetsList/Private/ViewportWidgetsListFunctionLibrary.cpp
+++ Source/ViewportWidgetsList/Private/ViewportWidgetsListFunctionLibrary.cpp
@@ -11,22 +11,23 @@ UWidget* UViewportWidgetsListFunctionLibrary::FindParentWidgetOfType(UWidget* St
 {
     if (StartingWidget)
     {
-        UPanelWidget* ParentPanel = StartingWidget->GetParent();
-        if (ParentPanel)
+        UWidget* ParentWidget = Cast<UWidget>(StartingWidget->GetParent());
+        if (ParentWidget)
         {
-            UWidget* ParentWidget = Cast<UWidget>(ParentPanel);
-            if (ParentWidget)
+            if (ParentWidget->IsA(Type))
             {
-                if (ParentWidget->IsA(Type))
-                {
-                    return ParentWidget;
-                }
-                return FindParentWidgetOfType(ParentWidget, Type);
+                return ParentWidget;
             }
+            return FindParentWidgetOfType(ParentWidget, Type);
         }
         UObject* OuterObject = StartingWidget->GetOuter();
         if (OuterObject)
         {
+            UWidgetTree* WidgetTree = Cast<UWidgetTree>(OuterObject);
+            if (WidgetTree == nullptr)
+            {
+                return nullptr;
+            }
             UWidget* ParentUserWidget = Cast<UUserWidget>(OuterObject->GetOuter());
             if (ParentUserWidget && ParentUserWidget->IsA(Type))
             {
